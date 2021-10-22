0a53f2a09105643363c804abce399ffc49ec355e
Miroslav Stojakovic <miroslav.miki.stojakovic@gmail.com>
2021-10-22 16:07:03 +0200

    Added properties to Acitivity migration

---
+++ Persistence/Migrations/20211022140618_AddedPropertiesToActivityDomain.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddedPropertiesToActivityDomain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Currency",
+                table: "Activities",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Activities",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Unit",
+                table: "Activities",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Currency",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "Unit",
+                table: "Activities");
+        }
+    }
+}
